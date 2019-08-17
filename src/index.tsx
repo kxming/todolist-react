7414f263352d7cf9c21aae5b2871049c3f5ea19b
kxming <964579219@qq.com>
2019-08-17 20:01:15 +0800

    添加第三方库

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './index.less';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// 顶部进度条
+import nProgress from "nprogress";
+// 富文本编辑器
+import BraftEditor from 'braft-editor'
+import 'braft-editor/dist/index.css'
+// emoji
+import { Picker } from 'emoji-mart'
+import 'emoji-mart/css/emoji-mart.css'
+// 高德地图
+import { Map } from 'react-amap';
+// 右键菜单
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
+// markdown编辑器
+const ReactMarkdown = require('react-markdown')
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
